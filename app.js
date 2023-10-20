8918e860eab2926d71e27f6cb32d466006b0f899
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-10-20 14:08:42 +0300

    Adding some styling

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 import { planets } from "./objects.js";
 import { iconSvg } from "./objects.js";
+import { starterTexts } from "./texts.js";
 
 const planetList = document.getElementById("planet-list");
 const modal = document.getElementById("info-modal");
 const backdrop = document.querySelector(".backdrop");
 const loadingSpinner = document.getElementById("loading-spinner");
+const aboutParagraphs = document.querySelectorAll(
+  ".start-paragraphs-unvisible"
+);
+const aboutTextContainer = document.getElementById("about-text-container");
+const starterButton = document.getElementById("starter-buttons").children[0];
+const readChroniclesButton = document.getElementById("read-chronicles-button");
 
 loadingSpinner.innerHTML = `
 <div>
 ${iconSvg}
 </div>
-`
+`;
+
+let textNumber = 0;
+
+const changeText = () => {
+    starterButton.className = 'button-unvisible'
+  aboutTextContainer.style.display = "block";
+  aboutTextContainer.className = "container-visible";
+  aboutParagraphs.forEach(function (paragraph, index) {
+    setTimeout(function () {
+      paragraph.className = "to-visible";
+    }, 550 + index * 750);
+  });
+};
 
 
 
@@ -22,22 +42,27 @@ const hideModal = () => {
   modal.style.display = "none";
 };
 
-const fetchPlanetData = async(name) => {
-    loadingSpinner.style.display = 'block';
-    const response = await fetch(`https://dom-planets-default-rtdb.firebaseio.com/planets/${name}.json`)
-    const data = await response.json();
-    loadingSpinner.style.display = 'none';
-    return data;
-}
-
+const fetchPlanetData = async (name) => {
+  loadingSpinner.style.display = "block";
+  const response = await fetch(
+    `https://dom-planets-default-rtdb.firebaseio.com/planets/${name}.json`
+  );
+  const data = await response.json();
+  loadingSpinner.style.display = "none";
+  return data;
+};
 
 backdrop.addEventListener("click", () => {
   toggleBackdrop();
   hideModal();
 });
 
+starterButton.addEventListener("click", changeText);
+
+
 planets.map((e) => {
   const newLi = document.createElement("li");
+  newLi.className = "planet-button";
   newLi.id = e.id;
   newLi.innerHTML = `<div id= ${e.name.toLowerCase()} class= planet-item >
         <h3>${e.name}</h3>
@@ -46,12 +71,12 @@ planets.map((e) => {
         </div>
     </div>`;
   planetList.appendChild(newLi);
-  const fixedName = e.name.toLowerCase()  
+  const fixedName = e.name.toLowerCase();
 
-    if(fixedName !== 'mercury') {
-        return;
-    }
-  newLi.addEventListener("click", async() => {
+  if (fixedName !== "mercury") {
+    return;
+  }
+  newLi.addEventListener("click", async () => {
     toggleBackdrop();
     const data = await fetchPlanetData(fixedName);
     modal.style.display = "block";
@@ -95,26 +120,39 @@ planets.map((e) => {
     </div>`;
     const closeButtton = document.querySelector(".close-button");
     closeButtton.addEventListener("click", () => {
-        toggleBackdrop();
-        hideModal();
-    })
+      toggleBackdrop();
+      hideModal();
+    });
     const seeInfoButton = document.querySelector(".see-info");
     const closeInfoButton = document.querySelector(".close-info");
     closeInfoButton.addEventListener("click", () => {
-        const infoTable = document.querySelector('.info-table');
-        infoTable.style.display = 'none';
-        seeInfoButton.style.display ="block";
-    })
+      const infoTable = document.querySelector(".info-table");
+      infoTable.style.display = "none";
+      seeInfoButton.style.display = "block";
+    });
     seeInfoButton.addEventListener("click", () => {
-        seeInfoButton.style.display ="none";
-        const infoTable = document.querySelector('.info-table');
-        infoTable.style.display = 'flex';
-        infoTable.style.flexDirection = 'column'
-        infoTable.style.gap = '1rem'
-    })
+      seeInfoButton.style.display = "none";
+      const infoTable = document.querySelector(".info-table");
+      infoTable.style.display = "flex";
+      infoTable.style.flexDirection = "column";
+      infoTable.style.gap = "1rem";
+    });
   });
 });
 
-const planetListItems = planetList.children;
 
 
+const showList = () => {
+    aboutTextContainer.className = "container-unvisible";
+    setTimeout(() => {
+        planetList.className = "planet-list-visible";
+        const planets = document.querySelectorAll(".planet-button");
+        console.log(planets);
+        planets.forEach(function (planet, index) {
+            setTimeout(function () {
+              planet.className = "planet-button-visible";
+            }, 550 + index * 750);
+          });
+    }, 550)
+}
+readChroniclesButton.addEventListener("click", showList);
---
+++ texts.js
@@ -0,0 +1,11 @@
+export const starterTexts = [
+  "Welcome, travelers! I'm Hakan, your celestial guide on this extraordinary journey through space. Join me as we navigate the cosmos and explore the wonders of our Solar System in Hakans Celestial Chronicles.",
+
+  "In this app, each planet holds a unique story waiting to be uncovered. From the fiery landscapes of Mercury to the icy mysteries of Neptune, I have documented my personal experiences and observations for each celestial body.",
+
+  "Feel free to dive into the diaries by clicking on the planet icons. Discover the secrets, beauty, and history that make each stop in our cosmic adventure truly special.",
+
+  "Are you ready to embark on a stellar voyage? Click on the icons, and let Hakans Celestial Chronicles be your guide to the wonders of the universe.",
+
+  "Look up to the stars,Hakan.",
+];
